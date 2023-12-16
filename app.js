0f66223aaa81e7427813596c570a08f873ef5ee5
PC <truongvanhao159@gmail.com>
2023-12-16 14:53:15 +0700

    modify deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("/api/names", (req, res) => {
   res.send("Nguyễn Văn B");
@@ -14,6 +14,6 @@ app.get("/", (req, res) => {
   res.send("Nguyễn Văn B");
 });
 
-app.listen(3000,  '0.0.0.0', () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server is runnint on port 3000");
 });
